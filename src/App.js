0e5f9c8693701b565608037f722f0895c8d7d9cf
fmomoreira <felipemoreira.frontend@gmail.com>
2022-11-16 10:07:20 -0300

    applyning_sideBarMenu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { Sidebar, Menu, MenuItem, useProSidebar, SubMenu } from 'react-pro-sidebar';
+
 
 function App() {
+  const { collapseSidebar } = useProSidebar();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div style={{ display: 'flex', height: '100%', position: 'absolute', zIndex: '9' }}>
+        <Sidebar>
+          <Menu>
+            <SubMenu label="Charts">
+              <MenuItem> Pie charts </MenuItem>
+              <MenuItem> Line charts </MenuItem>
+            </SubMenu>
+            <MenuItem> Documentation </MenuItem>
+            <MenuItem> Calendar </MenuItem>
+          </Menu>
+        </Sidebar>
+        <main>
+          <button onClick={() => collapseSidebar()}>Collapse</button>
+        </main>
+      </div>
+      <div className="App" style={{ marginLeft: '80px', position: 'relative'}}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
